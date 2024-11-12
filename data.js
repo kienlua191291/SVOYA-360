a2c93710ad4dbf4376c5885ce23e8c48b33a3a9f
kienlua191291 <81843114+kienlua191291@users.noreply.github.com>
2024-11-12 10:24:17 +0700

    upload

--- data.js
+++ data.js
@@ -1,46 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-b01",
-      "name": "B01",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1500,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": -1.606646087486446,
-          "pitch": 0.1309193888959168,
-          "rotation": 0,
-          "target": "1-b02"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "1-b02",
-      "name": "B02",
+      "id": "0-d18",
+      "name": "D18",
       "levels": [
         {
           "tileSize": 256,
